ff84efb3bc3b62ddb8e62c404c6bdc603bf18023
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2025-09-27 11:23:11 +0900

    adding stats

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import chart_counts, count_chars, count_words
 
 def get_book_text(path):
     with open(path, 'r', encoding='utf-8') as f:
@@ -9,5 +9,7 @@ def main():
     book_text = get_book_text('books/frankenstein.txt')
     word_count = count_words(book_text)
     print(f'Found {word_count} total words')
+    char_dict = count_chars(book_text)
+    print(chart_counts(char_dict))
  
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_chars(text):
+    char_dict = {}
+    for char in text:
+        char = char.lower()
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
