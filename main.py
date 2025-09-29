005692fd7aa130bf0bb4ddd0607585560aa57d08
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2025-09-29 09:19:15 +0900

    more books

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_chars, count_words, sort_char_counts
+import sys
 
 def get_book_text(path):
     with open(path, 'r', encoding='utf-8') as f:
@@ -6,14 +7,18 @@ def get_book_text(path):
     return ""
 
 def main():
-    book_text = get_book_text('books/frankenstein.txt')
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+    path = sys.argv[1]
+
+    book_text = get_book_text(path)
     word_count = count_words(book_text)
     print(f'Found {word_count} total words')
     char_dict = count_chars(book_text)
     stats = sort_char_counts(char_dict)
  
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}")
     print("----------- Word Count ----------")
     print(f'Found {word_count} total words')
     print("--------- Character Count -------")
